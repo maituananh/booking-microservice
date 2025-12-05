f197695a6f4f041ebc4c7b86e125ab02816cf4a1
anhmt <maituananh12061998@gmail.com>
2025-12-05 21:49:06 +0700

    feat: Create mutil modules

---
+++ booking-service/src/main/java/org/booking/Main.java
@@ -0,0 +1,10 @@
+package org.booking;
+
+@SpringBootApplication
+public class BookingMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingMicroserviceApplication.class, args);
+    }
+
+}
