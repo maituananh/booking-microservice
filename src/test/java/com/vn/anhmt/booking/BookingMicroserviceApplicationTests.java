fc7f2ccf55f279d93b0d3dac9eb284c4fdd36043
anhmt <maituananh12061998@gmail.com>
2025-12-05 21:11:16 +0700

    first commit

---
+++ src/test/java/com/vn/anhmt/booking/BookingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vn.anhmt.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
