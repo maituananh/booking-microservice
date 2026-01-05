388c369112cad284e5a8b466cda0ca4640cf7899
AnhMaiTuan <amaituan@openwt.com>
2026-01-05 15:52:17 +0700

    feat: Circuitbreaker + retry

--- order-service/src/main/java/org/booking/infra/adapter/InventoryClient.java
+++ order-service/src/main/java/org/booking/infra/adapter/InventoryClient.java
@@ -1,17 +1,23 @@
 package org.booking.infra.adapter;
 
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(
-    value = "inventoryService",
-    name = "inventoryService",
-    url = "http://localhost:8081",
-    fallback = InventoryClientFallback.class)
+@FeignClient(value = "inventoryService", name = "inventoryService", url = "http://localhost:8081")
 public interface InventoryClient {
 
+  @Retry(name = "inventoryService", fallbackMethod = "fallbackGetProduct")
+  @CircuitBreaker(name = "inventoryService")
   @GetMapping("/api/inventories/{id}")
   String getProductById(@PathVariable final UUID id);
+
+  // Must use CallNotPermittedException, if not it always return fallbackMethod
+  default String fallbackGetProduct(UUID id, CallNotPermittedException t) {
+    return "OUT_OF_STOCK";
+  }
 }
